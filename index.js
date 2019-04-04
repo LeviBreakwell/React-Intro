b2657f57c2691c1c641a174bb9d8a71f11a3cb97
LeviBreakwell <49215725+LeviBreakwell@users.noreply.github.com>
2019-04-04 11:30:04 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function LikeCount() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(oldCount => oldCount + 1);
+  };
+  const decrement = () => {
+    setCount(oldCount => oldCount - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={increment}>Like</button>
+      <button onClick={decrement}>Dislike</button>
+      <p>Overall Count: {count}</p>
+    </div>
+  );
+}
+function Headline(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <LikeCount />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Headline title="Likes" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
